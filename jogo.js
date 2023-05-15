fd9e0dfbbe5abdd98e129d9ffa0127f5e3f95f81
João Lavor <joaolavor15gmail.com>
2023-05-15 16:04:01 -0300

    Criando posições randômicas

--- jogo.js
+++ jogo.js
@@ -1,12 +1,37 @@
-
+//Ajustando a resolução do jogo de acordo com a aperelho usado 
 var altura = 0
 var largura = 0
 
-function ajustaTamanhoPalcoJogo () {
-        altura = window.innerHeight
-        largura = window.innerWidth
-       
-        console.log(altura,largura)
+function ajustaTamanhoPalcoJogo() {
+    altura = window.innerHeight
+    largura = window.innerWidth
+
+    console.log(altura, largura)
 }
 
 ajustaTamanhoPalcoJogo()
+
+//Criando mosquitos de forma aleatória na tela 
+//(Math.floor = tirar casais decimais)
+
+function posicaoRandomica() {
+    var posicaoX = Math.floor(Math.random() * largura) - 90
+    var posicaoY = Math.floor(Math.random() * altura) - 90
+
+    posicaoX = posicaoX < 0 ? 0 : posicaoX
+    posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+    console.log(posicaoX, posicaoY)
+
+
+    //Criando elementos html usando DOM
+
+    var mosquito = document.createElement('img')
+    mosquito.src = 'imagens/mosquito.png'
+    mosquito.className = 'mosquito1'
+    mosquito.style.left = posicaoX + 'px'
+    mosquito.style.top = posicaoY + 'px'
+    mosquito.style.position = 'absolute'
+
+    document.body.appendChild(mosquito)
+}
