6e79542e55ce86826bed1daa82c4789ebcd55666
João Lavor <107010223+JoaoLavor@users.noreply.github.com>
2023-05-15 16:44:51 -0300

    Criando posições randômicas
    Criação do estilo,css e colocando posições randômicas no game

--- jogo.js
+++ jogo.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 var altura = 0
 var largura = 0
@@ -9,4 +10,17 @@ function ajustaTamanhoPalcoJogo () {
         console.log(altura,largura)
 }
 
+=======
+
+var altura = 0
+var largura = 0
+
+function ajustaTamanhoPalcoJogo () {
+        altura = window.innerHeight
+        largura = window.innerWidth
+       
+        console.log(altura,largura)
+}
+
+>>>>>>> e13d01a (Iniciando Projeto)
 ajustaTamanhoPalcoJogo()
