a7ec2859f2bd75ef343208b47aa3b6411d8f5536
João Lavor <107010223+JoaoLavor@users.noreply.github.com>
2023-05-16 16:34:52 -0300

    Criando e removendo mosquitos a cada ciclo de tempo
    Colocando a função de remoção e criação de mosquitos em um certo período de tempo

--- jogo.js
+++ jogo.js
@@ -15,6 +15,11 @@ ajustaTamanhoPalcoJogo()
 //(Math.floor = tirar casais decimais)
 
 function posicaoRandomica() {
+
+    //removendo mosquito anterior (caso exista) 
+    if (document.getElementById('mosquito')) {
+            document.getElementById('mosquito').remove()
+    }
     var posicaoX = Math.floor(Math.random() * largura) - 90
     var posicaoY = Math.floor(Math.random() * altura) - 90
 
@@ -33,6 +38,7 @@ function posicaoRandomica() {
     mosquito.style.left = posicaoX + 'px'
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
+    mosquito.id = 'mosquito'
 
     document.body.appendChild(mosquito)
 }
